b3cb6a39116b70587b1791491f1df9edf5f96225
Alby Alex <albysalex@gmail.com>
2023-02-01 12:55:12 -0500

    recent changes

--- firstIter.py
+++ firstIter.py
@@ -9,9 +9,36 @@ spotify = tk.Spotify(user_token)
 
 playlist = spotify.playlist("4TEfGWKFu6u9b7pwZNd2oM")
 
-new_playlist = spotify.playlist_create(spotify.current_user().id, "{} (clean)".format(playlist.name)).id
 
-for i in playlist.tracks.items:
-    if not i.track.explicit:
-        spotify.playlist_add(new_playlist, [i.track.uri])
-    # print(i.track)
+
+to_add_uris = []
+
+#     for i in playlist.tracks.items:
+#         if not i.track.explicit:
+#             to_add_uris.append(i.track.uri)
+#         else:
+#             compare_artist = set()
+#             for j in i.track.artists:
+#                 compare_artist.add(j.name)
+#             result = spotify.search("track:{}".format(i.track.name), types=['track'], limit = 20)
+#             success = False
+#             for j in result[0].items:
+#                 if not j.explicit:
+#                     temp = set()
+#                     for k in j.artists:
+#                         temp.add(k.name)
+#                     if temp == compare_artist:
+#                         # print("inital version: {} found version: {}".format(i.track.name, j.name))
+#                         success = True
+#                         break
+#             if not success:
+#                 print("Failed: {}".format(i.track.name))
+
+
+
+# # result = spotify.search("track:No Brainer", types=['track'], limit = 5)
+# # for i in result[0].items:
+# #     print(i)
+# new_playlist = spotify.playlist_create(spotify.current_user().id, "{} (clean)".format(playlist.name)).id
+
+# spotify.playlist_add(new_playlist, to_add_uris)
